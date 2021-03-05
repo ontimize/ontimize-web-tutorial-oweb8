10aa2c2418797f26663d242e27785bc34093d2bf
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2021-03-05 16:05:58 +0100

    Exercise 17.1
    (cherry picked from commit c596679979fc7005158f8a3d877773a1145501fc)

--- src/app/main/customers/customers-detail/customers-detail.component.ts
+++ src/app/main/customers/customers-detail/customers-detail.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+import { OTableComponent } from 'ontimize-web-ngx';
 import { intRateMonthlyFunction } from '../../../shared/shared.module';
 
 @Component({
   selector: 'app-customers-detail',
   templateUrl: './customers-detail.component.html',
-  styleUrls: ['./customers-detail.component.css']
+  styleUrls: ['./customers-detail.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    '[class.customer-detail-form]': 'true'
+  }
 })
 export class CustomersDetailComponent implements OnInit {
 
+  @ViewChild('accountCustomerTable', { static: false }) accountTable: OTableComponent;
   public intRateMonthly = intRateMonthlyFunction;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
+
+  public openAccountDetailSelected() {
+    let selected = this.accountTable.getSelectedItems();
+    if (selected.length === 1) {
+      let accountId = selected[0]['ACCOUNTID']
+      this.router.navigate(['main/accounts/' + accountId]);
+    }
+  }
 
   ngOnInit() {
   }
